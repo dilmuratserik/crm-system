7d2cb0e346613db8ffcca8d338f83d520a0e9af3
dilmurat <dilmuratserikov@gmail.com>
2020-05-28 08:56:27 +0600

    fixed-change

--- client/e2e/protractor.conf.js
+++ client/e2e/protractor.conf.js
@@ -12,7 +12,7 @@ exports.config = {
     'browserName': 'chrome'
   },
   directConnect: true,
-  baseUrl: '/',
+  baseUrl: 'http://localhost:4200/',
   framework: 'jasmine',
   jasmineNodeOpts: {
     showColors: true,
