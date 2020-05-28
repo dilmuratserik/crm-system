152dc9bc3103d39f258f78dc326de41f94da3a5c
dilmurat <dilmuratserikov@gmail.com>
2020-05-28 09:22:18 +0600

    fixed-change3

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
 const  app = require('./app')
-const port = process.env.PORT | 42848
-app.listen(port,()=>console.log(`Server has been started on ${port}`))
+const port = process.env.PORT || 5000
+const host = process.env.localhost|| '0.0.0.0'
+app.listen(port,host,()=>console.log(`Server has been started on ${port}`))
